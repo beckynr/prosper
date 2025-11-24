6aff36984a01e9c5ef605dabcf1a4972c15d082f
Becky Norling-Ruggles <becky.norlingruggles@gmail.com>
2025-11-23 21:27:53 -0500

    trying db connection

--- app/main.py
+++ app/main.py
@@ -1,17 +1,42 @@
-import os
+import flask
 
-from flask import Flask, render_template, request
+import functions_framework
 
-app = Flask(__name__)
+from app import get_index_context, init_connection_pool, migrate_db, save_vote
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+############ TABS vs. SPACES App for Cloud Functions ############
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    user_input = request.form['user_input']
-    return f"You entered: {user_input}"
+# lazy global initialization
+# db connection must be established within request context
+db = None
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@functions_framework.http
+def votes(request: flask.Request) -> flask.Response:
+    """Handles HTTP requests to our application.
+
+    Args:
+        request: a Flask request object.
+
+    Returns:
+        Flask HTTP Response to the client.
+    """
+    global db
+    # initialize db within request context
+    if not db:
+        # initiate a connection pool to a Cloud SQL database
+        db = init_connection_pool()
+        # creates required 'votes' table in database (if it does not exist)
+        migrate_db(db)
+    if request.method == "GET":
+        context = get_index_context(db)
+        return flask.render_template("index.html", **context)
+
+    if request.method == "POST":
+        team = request.form["team"]
+        return save_vote(db, team)
+
+    return flask.Response(
+        response="Invalid http request. Method not allowed, must be 'GET' or 'POST'",
+        status=400,
+    )
